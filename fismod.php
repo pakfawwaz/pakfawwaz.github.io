705aee5be7e1bc362f59fce4c33a6769d5bbb0d5
pakfawwaz <54930596+pakfawwaz@users.noreply.github.com>
2020-04-11 22:55:50 +0700

    php

---
+++ fismod.php
@@ -0,0 +1,87 @@
+<?php
+
+class relativitas{
+    function kecepatan (){
+        $a;
+        $b;
+        $c=$a+$b;
+        $d=$a * $b;
+        $e=$d/;
+        $f=1+$e;
+        $g=$c/$f;
+        echo($g);
+
+    }
+    function panjang (){
+        $lo;
+        $v;
+        $vperc=
+
+    }
+    function waktu(){
+
+    }
+    function massa(){
+
+    }
+    function ek_relativitas(){
+
+    }
+    if(){
+        kecepatan()
+    }
+    elseif(){
+        panjang()
+    }
+    elseif(){
+        waktu()
+
+    }
+    elseif(){
+        massa()
+    }
+    else{
+        ek_relativitas()
+    }
+}
+
+class Energi_foton{
+    function lamda (){
+
+    }
+    function frekwensi (){
+
+    } 
+
+    if(){
+        lamda ()
+    }
+    else{
+        frekwensi ()
+    }
+
+}
+class Efek_fotolistrik{
+    function frekuensi(){
+
+    }
+    function lamda(){
+
+    }
+
+}
+class Efek_compton{
+    function lamda1(){
+
+    }
+    function lamda2(){
+
+    }
+    function m (){
+
+    }
+    function teta (){
+
+    }
+
+}
