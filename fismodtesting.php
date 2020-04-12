0e7f9ee6711bf72c3d6fad09e42c59618c1ad9f0
pakfawwaz <54930596+pakfawwaz@users.noreply.github.com>
2020-04-12 17:31:48 +0700

    test

--- fismodtesting.php
+++ fismodtesting.php
@@ -1,9 +1,38 @@
 <!DOCTYPE html>
 <?php
-$message = "";
-if(isset($_POST['SubmitButton'])){ //check if form was submitted
-  $input = $_POST['inputText']; //get input text
-  $message = "Success! You entered: ".$input;
-  echo $message;
-}    
-?>
+
+$umur = $_GET['umur'];
+$nama = $_GET['name'];
+$email = $_GET['email'];
+
+
+
+echo "<br>";
+echo "Usia".$umur;
+echo "<br>";
+echo "Email".$email;
+echo "<br>";
+
+
+class perulangan {
+    
+    function fungsiumur($umur){
+        for ($x = 0; $x <= $umur; $x++) {
+            echo "Hehehehe $x <br>";
+        }
+        if($umur<25){
+            echo "Tobat kamu masih polos";
+            echo "<br>";
+        } else {
+            echo "Halo";
+            echo "<br>";
+        }
+    }
+    function fungsinama($nama){
+        echo "Halo".$nama;
+    }
+}
+$y = new perulangan();
+
+echo $y -> fungsiumur($umur);
+echo $y -> fungsinama($nama);
