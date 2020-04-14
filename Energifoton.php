58a1d4beb490da46026ab998c856a6f505dea2e0
pakfawwaz <54930596+pakfawwaz@users.noreply.github.com>
2020-04-14 20:45:15 +0700

    Update Energifoton.php

--- Energifoton.php
+++ Energifoton.php
@@ -14,18 +14,18 @@ $konstanta_c=3*$x2;
 class Energi_foton{
     function lamda ($lamda){
         $x1=10**-34;
-$x2=10**8;
-$konstanta_plank=6.6*$x1;
-$konstanta_c=3*$x2;
+        $x2=10**8;
+        $konstanta_plank=6.6*$x1;
+        $konstanta_c=3*$x2;
         $brow=$konstanta_plank*$konstanta_c/$lamda*10**-10;
         echo ($brow);
 
     }
     function frekwensi ($frekuensi){
         $x1=10**-34;
-$x2=10**8;
-$konstanta_plank=6.6*$x1;
-$konstanta_c=3*$x2;
+        $x2=10**8;
+        $konstanta_plank=6.6*$x1;
+        $konstanta_c=3*$x2;
         $bruh=$konstanta_plank*$frekuensi;
         echo ($bruh);
 
@@ -35,9 +35,9 @@ $konstanta_c=3*$x2;
 }
 $hore= new Energi_foton();
 
-if($soal=1){
+if($soal=2){
     echo $hore -> lamda ($lamda);
 }
-else{
+else if ($soal=1){
     echo $hore -> frekwensi ($frekuensi);
 }
