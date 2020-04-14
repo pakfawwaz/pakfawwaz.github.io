65c40f992e3b81ede3f7f10040ffd351281abc4d
pakfawwaz <54930596+pakfawwaz@users.noreply.github.com>
2020-04-14 23:38:03 +0700

    relatifitas massa

--- fismod.php
+++ fismod.php
@@ -4,6 +4,9 @@
 $frekuensi = $_POST['frekuensi'];
 $lamda = $_POST['lamda'];
 $soal = $_POST['soal'];
+$m_relatif = $_POST['m_relatif'];
+$m_bumi = $_POST['m_bumi'];
+$m_kec = $_POST['m_kec'];
 $x1=10**-34;
 $x2=10**8;
 $konstanta_plank=6.6*$x1;
@@ -30,8 +33,34 @@ class Energi_foton{
 
     }
 }
+class relativitas_massa{
+    function massa_relatif($m_bumi,$m_kec){
+        $x2=10**8;
+        $konstanta_c=3*$x2;
+        $ayam = $m_kec**2;
+        $konstanta_c_2 = $konstanta_c**2;
+        $goreng = $ayam/$konstanta_c_2;
+        $gorengan = 1-$goreng;
+        $pisang = sqrt($gorengan);
+        $broski = $m_bumi/$pisang;
+        echo ($broski);
+    }
+    function massa_bumi($m_relatif,$m_kec){
+        $x2=10**8;
+        $konstanta_c=3*$x2;
+        $ayam = $m_kec**2;
+        $konstanta_c_2 = $konstanta_c**2;
+        $goreng = $ayam/$konstanta_c_2;
+        $gorengan = 1-$goreng;
+        $pisang = sqrt($gorengan);
+        $nani = $m_relatif*$pisang;
+        echo($broski);
+    }
+
+}
 
 $hore= new Energi_foton();
+$hore4= new relativitas_massa();
 
 if($soal=="1"){
     
@@ -40,4 +69,13 @@ if($soal=="1"){
 else if ($soal=="2"){
     
     echo $hore -> lamda ($lamda);
+}
+
+if($soal4 =="1"){
+    
+    echo $hore4 -> massa_relatif ($m_bumi,$m_kec);
+}
+else if ($soal4 =="2"){
+    
+    echo $hore4 -> massa_bumi ($m_relatif,$m_kec);
 }
