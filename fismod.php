162bc9420ed9cc1d963193d2bf0a723c65b6da70
pakfawwaz <54930596+pakfawwaz@users.noreply.github.com>
2020-04-15 01:54:22 +0700

    FISIKAMODERN SELESAI LURD

--- fismod.php
+++ fismod.php
@@ -27,36 +27,87 @@ class Energi_foton{
 
     }
 }
+// class relativitas_kecepatan{
+//     function
+
+
+// }
+class relativitas_panjang{
+    function panjang_relatif($p_bumi,$p_kec){
+        $ayam = $p_kec**2;
+        $gorengan = 1- $ayam;
+        $pisang = sqrt($gorengan);
+        $sans = $p_bumi*$pisang;
+        echo($sans);
+
+
+    }
+    function panjang_bumi($p_relatif,$p_kec){
+        $ayam = $p_kec**2;
+        $gorengan = 1- $ayam;
+        $pisang = sqrt($gorengan);
+        $santuy = $p_relatif/$pisang;
+        echo($santuy);
+
+    }
+
+}
+class relativitas_waktu{
+    function waktu_relatif($w_bumi,$w_kec){
+        $ayam = $w_kec**2;
+        $gorengan = 1- $ayam;
+        $pisang = sqrt($gorengan);
+        $broski = $w_bumi/$pisang;
+        echo ($broski);
+        
+    }
+    function waktu_bumi ($w_relatif,$w_kec){
+        $ayam = $w_kec**2;
+        $gorengan = 1- $ayam;
+        $pisang = sqrt($gorengan);
+        $nani = $w_relatif*$pisang;
+        echo($nani);
+
+    }
+    
+
+}
 class relativitas_massa{
     function massa_relatif($m_bumi,$m_kec){
-        $x2=10**8;
-        $konstanta_c=3*$x2;
         $ayam = $m_kec**2;
-        $konstanta_c_2 = $konstanta_c**2;
-        $goreng = $ayam/$konstanta_c_2;
-        $gorengan = 1-$goreng;
+        $gorengan = 1- $ayam;
         $pisang = sqrt($gorengan);
         $broski = $m_bumi/$pisang;
         echo ($broski);
     }
     function massa_bumi($m_relatif,$m_kec){
-        $x2=10**8;
-        $konstanta_c=3*$x2;
         $ayam = $m_kec**2;
-        $konstanta_c_2 = $konstanta_c**2;
-        $goreng = $ayam/$konstanta_c_2;
-        $gorengan = 1-$goreng;
+        $gorengan = 1- $ayam;
         $pisang = sqrt($gorengan);
         $nani = $m_relatif*$pisang;
-        echo($broski);
+        echo($nani);
+    }
+
+}
+class energi_kinetik{
+    function energi_kinetik_($e_m_bumi,$e_kec){
+        $e_kec_2=$e_kec**2;
+        $ayam=1-$e_kec_2;
+        $gorengan=sqrt($ayam);
+        $x2=10**8;
+        $konstanta_c=3*$x2;
+        $konstanta_c_2=$konstanta_c**2;
+        $rumuseinstein = $e_m_bumi*$konstanta_c_2;
+        $E=$rumuseinstein/$gorengan;
+        $EK=$E-$rumuseinstein;
+        echo($EK);
+
     }
 
 }
 
-
-
-
-if ($_POST['soal']){
+if (isset($_POST['soal'])){
+    $soal=$_POST['soal'];
     $hore= new Energi_foton();
     if($soal=="1"){
         $frekuensi = $_POST['frekuensi'];
@@ -74,7 +125,49 @@ if ($_POST['soal']){
     }
 
 }
-elseif($_POST['soal4']){
+// elseif(isset($_POST['soal1'])){
+
+// }
+elseif(isset($_POST['soal2'])){
+    $soal2=$_POST['soal2'];
+    $hore2= new relativitas_panjang();
+    if($soal2 == "1"){
+        $p_relatif = $_POST['p_relatif'];
+        $p_bumi = $_POST['p_bumi'];
+        $p_kec = $_POST['p_kec'];
+        echo $hore2 -> panjang_relatif ($p_bumi,$p_kec);
+
+    }
+    else if ($soal2 == "2"){
+        $p_relatif = $_POST['m_relatif'];
+        $p_bumi = $_POST['m_bumi'];
+        $p_kec = $_POST['m_kec'];
+        echo $hore2 -> panjang_bumi ($p_relatif,$p_kec);
+
+    }
+
+}
+elseif(isset($_POST['soal3'])){
+    $soal3=$_POST['soal3'];
+    $hore3= new relativitas_waktu();
+    if($soal3 =="1"){
+        $w_relatif = $_POST['w_relatif'];
+        $w_bumi = $_POST['w_bumi'];
+        $w_kec = $_POST['w_kec'];
+        
+        echo $hore3 -> waktu_relatif ($w_bumi,$w_kec);
+    }
+    else if ($soal3 =="2"){
+        $w_relatif = $_POST['w_relatif'];
+        $w_bumi = $_POST['w_bumi'];
+        $w_kec = $_POST['w_kec'];
+        
+        echo $hore3 -> waktu_bumi ($w_relatif,$w_kec);
+    }
+
+}
+elseif(isset($_POST['soal4'])){
+    $soal4=$_POST['soal4'];
     $hore4= new relativitas_massa();
     if($soal4 =="1"){
         $m_relatif = $_POST['m_relatif'];
@@ -90,6 +183,15 @@ elseif($_POST['soal4']){
         
         echo $hore4 -> massa_bumi ($m_relatif,$m_kec);
     }
+
+}
+elseif(isset($_POST['soal5'])){
+    $soal5=$_POST['soal5'];
+    $hore5= new energi_kinetik();
+    $e_m_bumi = $_POST['e_m_bumi'];
+    $e_kec = $_POST['e_kec'];
+    echo $hore5 -> energi_kinetik_ ($e_m_bumi,$e_kec);
+
 
 }
 
