c0d80a69b1c6ae33e0ae622fcfa20748c542978c
pakfawwaz <54930596+pakfawwaz@users.noreply.github.com>
2020-04-14 20:28:48 +0700

    php

---
+++ relativitaskecepatan.php
@@ -0,0 +1,16 @@
+<?php
+
+class relativitaskecepatan{
+    function kecepatan (){
+        $a;
+        $b;
+        $c=$a+$b;
+        $d=$a * $b;
+        //$e=$d/;
+        $f=1+$e;
+        $g=$c/$f;
+        echo($g);
+
+    }
+}
+
