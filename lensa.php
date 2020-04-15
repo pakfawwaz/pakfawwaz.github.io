5ffad45885b83f7c176538b24248f2149a34af00
pakfawwaz <54930596+pakfawwaz@users.noreply.github.com>
2020-04-15 12:48:04 +0700

    lensa

---
+++ lensa.php
@@ -0,0 +1,37 @@
+<?php
+class lensa_cekung{
+    function fokus(){
+
+    }
+    function jarak_benda_k(){
+
+    }
+    function jarak_bayangan_k(){
+
+    }
+    function jari_lensa_k(){
+
+    }
+    function perbesaran(){
+
+    }
+
+}
+class lensa_cembung{
+    function fokus_b(){
+
+    }
+    function jarak_benda_b(){
+
+    }
+    function jarak_bayangan_b(){
+
+    }
+    function jari_lensa_b(){
+
+    }
+    function perbesaran_b(){
+        
+    }
+
+}
