f53b984d6bdf5ab58b544ac44681c532f0b311b6
pakfawwaz <54930596+pakfawwaz@users.noreply.github.com>
2020-04-16 20:25:16 +0800

    bug fix lensa

--- lensa.php
+++ lensa.php
@@ -106,23 +106,23 @@ else if (isset($_POST['soale1'])){
     $jari_lensa_b = $_POST['jari_lensa_b'];
     $halo1 = new lensa_cekung();
     if ($_POST['soale1'] == 1){
-        echo $halo -> fokus_b($jarak_bayangan_b,$jarak_benda_b);
+        echo $halo1 -> fokus_b($jarak_bayangan_b,$jarak_benda_b);
 
     }
     elseif ($_POST['soale1'] == 2) {
-        echo $halo -> jarak_benda_b($fokus_b,$jarak_bayangan_b);
+        echo $halo1 -> jarak_benda_b($fokus_b,$jarak_bayangan_b);
         # code...
     }
     elseif ($_POST['soale1'] == 3) {
-        echo $halo -> jarak_bayangan_b($fokus_b,$jarak_benda_b);
+        echo $halo1 -> jarak_bayangan_b($fokus_b,$jarak_benda_b);
         # code...
     }
     elseif ($_POST['soale1'] == 4) {
-        echo $halo -> jari_lensa_b($jarak_bayangan_b,$jarak_benda_b);
+        echo $halo1 -> jari_lensa_b($jarak_bayangan_b,$jarak_benda_b);
         # code...
     }
     elseif ($_POST['soale1'] == 5) {
-        echo $halo -> perbesaran_b($jarak_bayangan_b,$jarak_benda_b);
+        echo $halo1 -> perbesaran_b($jarak_bayangan_b,$jarak_benda_b);
         # code...
     }
 
